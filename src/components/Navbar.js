2db3a860bbd6201625fe06370046983e15fc6c9a
Agung Saputra <agungsaputradifh@gmail.com>
2023-01-03 11:30:39 +0700

    added logo to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
+import DXlogo2 from "./img/logo/DX-logo-2.png";
 
 export default function Navbar() {
   return (
     <nav className="flex justify-between items-center px-32 py-5 bg-[#00ADB5]">
       <div>
-        <h1 className="text-slate-900 text-3xl font-bold"> DETERMINIX</h1>
+        <img src={DXlogo2} width="85" />
       </div>
       <div className="flex gap-10 text-slate-900 text-xl">
         <Link to="/">home</Link>
