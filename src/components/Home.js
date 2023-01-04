4c7ac5cefe0affb4d827f69a7b1ccd0059364a60
Agung Saputra <agungsaputradifh@gmail.com>
2023-01-04 21:13:03 +0700

    new aris  at home

--- src/components/Home.js
+++ src/components/Home.js
@@ -17,7 +17,9 @@ export default function Home() {
         </button>
       </div>
       <div>
-        <img src={Aris} width="500" className="rounded-xl shadow-lg shadow-[#71C9CE]" />
+        <div className="relative bg-[#71C9CE] w-[455px] h-[257px] rounded-lg shadow-xl">
+          <img src={Aris} width="450" className="absolute -right-3 top-[14px] rounded-lg" />
+        </div>
       </div>
     </div>
   );
