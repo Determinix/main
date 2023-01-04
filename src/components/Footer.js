c846e2393f1b0b78e1781d243f85d3bb90213dbb
aru chevy <aruchevy31@gmail.com>
2023-01-04 13:38:28 +0700

    edit footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <div className="bg-black text-white">
+    <footer className="bg-black text-white pt-24 mt-10 lg:text-center">
       <h1>Ini footer</h1>
-    </div>
+    </footer>
   );
 }
