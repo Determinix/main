b1c6a5981fa4276f424c6d21d6569d7ef93833a9
Agung Saputra <agungsaputradifh@gmail.com>
2023-01-04 13:17:18 +0700

    added photo

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,8 +3,8 @@ export default function Home() {
     <div className="px-48 py-10">
       <div>
         <h1 className="text-5xl font-bold text-slate-900">DETERMINIX</h1>
-        <h1 className="text-xl font-bold text-slate-500">Who Is Determinix?</h1>
-        <span className="text-xl">Determinix is ​​a team that works in:</span>
+        <h1 className="text-xl font-bold text-slate-500">What Is Determinix?</h1>
+        <span className="text-xl">Determinix is ​a team that works in:</span>
         <ul className="list-disc text-xl pl-10">
           <li>Website Developer</li>
           <li>Software Developer (Coming Soon)</li>
