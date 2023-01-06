8258c4f2537b5b6c1d1c6c792c6817993871aad4
Agung Saputra <agungsaputradifh@gmail.com>
2023-01-06 10:20:48 +0700

    new project template

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,7 +1,84 @@
 export default function Agent() {
   return (
-    <div className="px-48">
-      <h1 className="text-red-500 py-56">Ini project</h1>
+    <div className="mb-28">
+      <h1 className="pt-20 pb-16 text-slate-900 font-bold text-4xl text-center">OUR PROJECT</h1>
+      <div className="mx-48 flex justify-between items-center">
+        <div className="relative w-[350px] h-[225px] bg-slate-200 shadow-xl">
+          <div className="absolute right-[10px] w-[330px] h-[160px] top-[10px] bg-slate-400 flex justify-center items-center text-5xl">???</div>
+          <div className="mx-[10px] absolute w-[325px] top-[180px] flex justify-between items-center">
+            <h1 className="text-xl">Nama Here</h1>
+            <button className="text-xl py-1 px-3 rounded-xl bg-yellow-400 hover:bg-yellow-600 text-slate-50">Live Preview</button>
+          </div>
+        </div>
+
+        <div className="relative w-[350px] h-[225px] bg-slate-200 shadow-xl">
+          <div className="absolute right-[10px] w-[330px] h-[160px] top-[10px] bg-slate-400 flex justify-center items-center text-5xl">???</div>
+          <div className="mx-[10px] absolute w-[325px] top-[180px] flex justify-between items-center">
+            <h1 className="text-xl">Nama Here</h1>
+            <button className="text-xl py-1 px-3 rounded-xl bg-yellow-400 hover:bg-yellow-600 text-slate-50">Live Preview</button>
+          </div>
+        </div>
+
+        <div className="relative w-[350px] h-[225px] bg-slate-200 shadow-xl">
+          <div className="absolute right-[10px] w-[330px] h-[160px] top-[10px] bg-slate-400 flex justify-center items-center text-5xl">???</div>
+          <div className="mx-[10px] absolute w-[325px] top-[180px] flex justify-between items-center">
+            <h1 className="text-xl">Nama Here</h1>
+            <button className="text-xl py-1 px-3 rounded-xl bg-yellow-400 hover:bg-yellow-600 text-slate-50">Live Preview</button>
+          </div>
+        </div>
+      </div>
+
+      <div className="mx-48 mt-7 flex justify-between items-center">
+        <div className="relative w-[350px] h-[225px] bg-slate-200 shadow-xl">
+          <div className="absolute right-[10px] w-[330px] h-[160px] top-[10px] bg-slate-400 flex justify-center items-center text-5xl">???</div>
+          <div className="mx-[10px] absolute w-[325px] top-[180px] flex justify-between items-center">
+            <h1 className="text-xl">Nama Here</h1>
+            <button className="text-xl py-1 px-3 rounded-xl bg-yellow-400 hover:bg-yellow-600 text-slate-50">Live Preview</button>
+          </div>
+        </div>
+
+        <div className="relative w-[350px] h-[225px] bg-slate-200 shadow-xl">
+          <div className="absolute right-[10px] w-[330px] h-[160px] top-[10px] bg-slate-400 flex justify-center items-center text-5xl">???</div>
+          <div className="mx-[10px] absolute w-[325px] top-[180px] flex justify-between items-center">
+            <h1 className="text-xl">Nama Here</h1>
+            <button className="text-xl py-1 px-3 rounded-xl bg-yellow-400 hover:bg-yellow-600 text-slate-50">Live Preview</button>
+          </div>
+        </div>
+
+        <div className="relative w-[350px] h-[225px] bg-slate-200 shadow-xl">
+          <div className="absolute right-[10px] w-[330px] h-[160px] top-[10px] bg-slate-400 flex justify-center items-center text-5xl">???</div>
+          <div className="mx-[10px] absolute w-[325px] top-[180px] flex justify-between items-center">
+            <h1 className="text-xl">Nama Here</h1>
+            <button className="text-xl py-1 px-3 rounded-xl bg-yellow-400 hover:bg-yellow-600 text-slate-50">Live Preview</button>
+          </div>
+        </div>
+      </div>
+
+      <div className="mx-48 mt-7 flex justify-between items-center">
+        <div className="relative w-[350px] h-[225px] bg-slate-200 shadow-xl">
+          <div className="absolute right-[10px] w-[330px] h-[160px] top-[10px] bg-slate-400 flex justify-center items-center text-5xl">???</div>
+          <div className="mx-[10px] absolute w-[325px] top-[180px] flex justify-between items-center">
+            <h1 className="text-xl">Nama Here</h1>
+            <button className="text-xl py-1 px-3 rounded-xl bg-yellow-400 hover:bg-yellow-600 text-slate-50">Live Preview</button>
+          </div>
+        </div>
+
+        <div className="relative w-[350px] h-[225px] bg-slate-200 shadow-xl">
+          <div className="absolute right-[10px] w-[330px] h-[160px] top-[10px] bg-slate-400 flex justify-center items-center text-5xl">???</div>
+          <div className="mx-[10px] absolute w-[325px] top-[180px] flex justify-between items-center">
+            <h1 className="text-xl">Nama Here</h1>
+            <button className="text-xl py-1 px-3 rounded-xl bg-yellow-400 hover:bg-yellow-600 text-slate-50">Live Preview</button>
+          </div>
+        </div>
+
+        <div className="relative w-[350px] h-[225px] bg-slate-200 shadow-xl">
+          <div className="absolute right-[10px] w-[330px] h-[160px] top-[10px] bg-slate-400 flex justify-center items-center text-5xl">???</div>
+          <div className="mx-[10px] absolute w-[325px] top-[180px] flex justify-between items-center">
+            <h1 className="text-xl">Nama Here</h1>
+            <button className="text-xl py-1 px-3 rounded-xl bg-yellow-400 hover:bg-yellow-600 text-slate-50">Live Preview</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
