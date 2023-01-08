5c3b82bcf767f7f0a0cca3e02f6ff8f4b234629f
Agung Saputra <agungsaputradifh@gmail.com>
2023-01-08 20:35:36 +0700

    fix bug and fix start

--- src/components/Agent.js
+++ src/components/Agent.js
@@ -1,14 +1,19 @@
+import Agung from "./img/profile/Agung.jpg";
+
 export default function Agent() {
   return (
-    <div className="px-48">
-      <span className="font-bold text-red-500">Determinix Agents</span>
-      <ol>
-        <li>Agung Saputra</li>
-        <li>Aru chevy Rasyid</li>
-        <li>Salsabil Albi Fahri</li>
-        <li>Krisna Setyo Ramadhan</li>
-        <li>Wisnu Hartono</li>
-      </ol>
+    <div>
+      <h1 className="pt-20 font-bold text-slate-900 text-4xl text-center">DX AGENTS</h1>
+      <div className="mx-auto w-[285px] h-[200px] shadow-2xl bg-slate-300">
+        <div className="flex items-center justify-between p-3">
+          <img src={Agung} alt="Agung" className="w-[100px] h-[100px]" />
+          <div>
+            <h1 className="text-xl text-center">Agung Saputra</h1>
+            <div className="w-[150px] h-[2px] bg-slate-900 mx-auto mt-[3px]"></div>
+            <h1 className="text-xl text-center">President</h1>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,12 +6,36 @@ export default function Navbar() {
     <nav>
       <div className="flex justify-between items-center px-48 bg-[#71C9CE]">
         <div>
-          <img src={DXlogo2} width="85" />
+          <img src={DXlogo2} alt="dxlogo" width="85" />
         </div>
         <div className="flex gap-10 text-slate-50 fontb text-xl">
-          <Link to="/">Home</Link>
-          <Link to="/Project">Project</Link>
-          <Link to="/Agent">Agent</Link>
+          <Link
+            to="/"
+            onClick={function hide() {
+              const home = document.querySelector("#home");
+              home.style.display = "none";
+            }}
+          >
+            Home
+          </Link>
+          <Link
+            to="/Project"
+            onClick={function hide() {
+              const home = document.querySelector("#home");
+              home.style.display = "none";
+            }}
+          >
+            Project
+          </Link>
+          <Link
+            to="/Agent"
+            onClick={function hide() {
+              const home = document.querySelector("#home");
+              home.style.display = "none";
+            }}
+          >
+            Agent
+          </Link>
         </div>
       </div>
       <div className="h-[6px] bg-yellow-400"></div>
