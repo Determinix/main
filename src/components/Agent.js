97f35535d177842e9454fecf9bd58ae9b50f17e2
Agung Saputra <agungsaputradifh@gmail.com>
2023-01-11 22:20:04 +0700

    fix tap the card agent

--- src/components/Agent.js
+++ src/components/Agent.js
@@ -6,9 +6,9 @@ export default function Agent() {
       <h1 className="pt-20 pb-14 font-bold text-slate-900 text-4xl text-center">DX AGENTS</h1>
 
       <div id="sosmed" className="flex justify-center items-center mb-8 mx-[420px] border-2 border-yellow-200 rounded-2xl text-slate-500">
-        <h1 className="pr-3 text-xl">tap the card to open social media</h1>
+        <h1 className="pr-3 text-xl">Tap the card to open social media</h1>
         <button
-          className="font-bold text-2xl"
+          className="font-bold text-red-600 text-2xl"
           onClick={function hide() {
             const sosmed = document.querySelector("#sosmed");
             sosmed.style.display = "none";
@@ -19,7 +19,7 @@ export default function Agent() {
       </div>
 
       <div className="flex justify-between items-center">
-        <a href="https://www.instagram.com/difhaels/?hl=id" target={"_blank"} className="w-[285px] h-[125px] shadow-2xl bg-slate-300 flex items-center">
+        <a href="https://www.instagram.com/difhaels/?hl=id" target={"_blank"} className="w-[285px] h-[125px] shadow-2xl bg-slate-300">
           <div className="flex items-center justify-between p-3">
             <img src={Agung} alt="Agung" className="w-[100px] h-[100px]" />
             <div>
