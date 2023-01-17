93467d5ecbadc6fcbb6c67111ef6c283b94e22eb
Agung Saputra <agungsaputradifh@gmail.com>
2023-01-17 22:58:09 +0700

    delete logo footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,6 @@ export default function Footer() {
   return (
     <footer className="flex justify-between bg-slate-900 text-white pt-24 lg:text-center">
       <div>
-        <img src={DXlogo2} alt="dxlogo" width="50" className="flex justify-between" />
         <h1 className="text-xl text-center">copyright 2023 create by determinix agent</h1>
       </div>
     </footer>
