2f6160861ef82dbd5f5f513ddced465629e9907f
Agung Saputra <agungsaputradifh@gmail.com>
2023-01-17 22:57:41 +0700

    change color footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import DXlogo2 from "./img/logo/DX-logo-2.png";
 
 export default function Footer() {
   return (
-    <footer className="flex justify-between bg-yellow-400 text-white pt-24 lg:text-center">
+    <footer className="flex justify-between bg-slate-900 text-white pt-24 lg:text-center">
       <div>
         <img src={DXlogo2} alt="dxlogo" width="50" className="flex justify-between" />
         <h1 className="text-xl text-center">copyright 2023 create by determinix agent</h1>
