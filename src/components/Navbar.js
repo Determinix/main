d0815cb144990e2205702d99f0fa7c09a9df9099
Agung Saputra <agungsaputradifh@gmail.com>
2023-01-18 21:57:58 +0700

    add new feature navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,11 +4,11 @@ import DXlogo2 from "./img/logo/DX-logo-2.png";
 export default function Navbar() {
   return (
     <nav className="fixed z-50 w-full">
-      <div className="flex justify-between items-center px-0 sm:px-48 bg-[#71C9CE]">
+      <div className="flex justify-between items-center sm:px-48 bg-[#71C9CE]">
         <div>
           <img src={DXlogo2} alt="dxlogo" width="85" />
         </div>
-        <div className="flex gap-10 text-slate-50 px-3 text-xl">
+        <div className="sm:flex gap-10 text-slate-50 px-3 text-xl items-center hidden">
           <Link
             to="/"
             onClick={function hide() {
@@ -36,6 +36,10 @@ export default function Navbar() {
           >
             Agent
           </Link>
+        </div>
+        <div className="grid gap-[6px] mx-3 sm:hidden">
+          <div className="bg-slate-200 w-[40px] h-[10px]"></div>
+          <div className="ml-[15px] bg-slate-100 w-[25px] h-[10px]"></div>
         </div>
       </div>
       <div className="h-[6px] bg-yellow-400"></div>
