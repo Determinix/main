59be45fb2f1f1a6ab160252b023587b7926d9516
Agung Saputra <agungsaputradifh@gmail.com>
2023-01-19 10:50:12 +0700

    added new feature navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,10 +4,11 @@ import DXlogo2 from "./img/logo/DX-logo-2.png";
 export default function Navbar() {
   return (
     <nav className="fixed z-50 w-full">
-      <div className="flex justify-between items-center sm:px-48 bg-[#71C9CE]">
+      <div className="flex justify-between items-center sm:px-48 bg-[#71C9CE] relative">
         <div>
           <img src={DXlogo2} alt="dxlogo" width="85" />
         </div>
+        {/* desktop */}
         <div className="sm:flex gap-10 text-slate-50 px-3 text-xl items-center hidden">
           <Link
             to="/"
@@ -37,7 +38,46 @@ export default function Navbar() {
             Agent
           </Link>
         </div>
-        <div className="grid gap-[6px] mx-3 sm:hidden">
+        {/* mobile */}
+        <div className="gap-3 py-2 rounded-md text-slate-50 px-3 text-xl items-center sm:hidden absolute bg-slate-600 right-3 top-24 hidden" id="nav">
+          <Link
+            to="/"
+            onClick={function hide() {
+              const home = document.querySelector("#home");
+              home.style.display = "none";
+            }}
+            className="block pb-1"
+          >
+            Home
+          </Link>
+          <Link
+            to="/Project"
+            onClick={function hide() {
+              const home = document.querySelector("#home");
+              home.style.display = "none";
+            }}
+            className="block pb-1"
+          >
+            Project
+          </Link>
+          <Link
+            to="/Agent"
+            onClick={function hide() {
+              const home = document.querySelector("#home");
+              home.style.display = "none";
+            }}
+            className="block"
+          >
+            Agent
+          </Link>
+        </div>
+        <div
+          className="grid gap-[6px] mx-3 sm:hidden"
+          onClick={function navMobile() {
+            const nav = document.querySelector("#nav");
+            nav.classList.toggle("hidden");
+          }}
+        >
           <div className="bg-slate-200 w-[40px] h-[10px]"></div>
           <div className="ml-[15px] bg-slate-100 w-[25px] h-[10px]"></div>
         </div>
