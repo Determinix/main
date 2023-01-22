6760a69d069de06140d8ac9afd59281bad722383
Agung Saputra <agungsaputradifh@gmail.com>
2023-01-22 23:01:38 +0700

    added template responsive

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,7 +3,7 @@ export default function Agent() {
     <div className="mb-28 pt-24 lg:mx-16 xl:mx-48">
       <h1 className="pt-20 pb-16 text-slate-900 font-bold text-4xl text-center">OUR PROJECT</h1>
 
-      <div className="grid sm:grid-cols-3 place-items-center gap-7 grid-cols-1">
+      <div className="grid sm:grid-cols-2 xl:grid-cols-3 mx-0 sm:mx-72 xl:mx-0 place-items-center gap-7 grid-cols-1">
         <div className="relative w-[350px] h-[225px] bg-slate-100 shadow-2xl">
           <div className="absolute right-[10px] w-[330px] h-[160px] top-[10px] bg-slate-400 flex justify-center items-center text-5xl">???</div>
           <div className="mx-[10px] absolute w-[325px] top-[180px] flex justify-between items-center">
