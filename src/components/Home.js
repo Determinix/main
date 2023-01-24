1a5e7a86aa0c9ad3beb7dffcc18e76347c254f40
Agung Saputra <agungsaputradifh@gmail.com>
2023-01-24 22:16:19 +0700

    new hero

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,33 +1,14 @@
-import Aris from "./img/logo/Aris.png";
 import Hina from "./img/logo/Hina.png";
 
 export default function Home() {
   return (
-    <div className="relative lg:px-36 2xl:px-80 px-5 pt-48 bg-gray-700">
-      <img src={Hina} className="opacity-30" />
-      <h1 className="text-5xl font-bold text-[#ff2c2c]">DETERMINIX</h1>
-      <h1 className="text-xl font-bold text-slate-500">What Is Determinix?</h1>
-      <span className="text-xl py-2">Determinix is ​a team that works in:</span>
-      <ul className="list-disc text-xl pl-10">
-        <li>Website Developer</li>
-        <li>Software Developer (Coming Soon)</li>
-        <li>Game Developer (Coming Soon)</li>
-      </ul>
+    <div className="relative lg:px-36 2xl:px-80 px-5 bg-gray-700 py-56 mb-96 grid place-items-center">
+      <img src={Hina} className="opacity-10 absolute bottom-0 right-48" alt="hina" />
+      <h1 className="text-[58px] font-bold text-[#ff2c2c]">DETERMINIX</h1>
+      <h1 className="text-white text-3xl pt-3">A team to study and work in the field of programmers. </h1>
+      <a className="bg-slate-100 px-5 py-3 text-2xl hover:bg-slate-400 mt-10 relative" href="https://reactjs.org/" target={"_blank"}>
+        Contact Us
+      </a>
     </div>
-    // <div className="relative mx-auto w-[900px] text-center bg-[#71C9CE] mt-32 rounded-t-2xl pb-[630px] z-20 hidden sm:flex">
-    //   <div className="absolute -top-10 left-[220px] text-3xl font-bold bg-slate-100 w-[50%] rounded-xl py-5 border-4 border-[#2bc2ca] shadow-lg z-10">Preview Project</div>
-    //   <div className="absolute top-[120px] -left-10 w-[600px] h-[320px] bg-slate-300 rounded-xl shadow-xl ">
-    //     Main Preview Here
-    //     <div className="absolute w-[400px] py-5 left-10 -bottom-10 bg-yellow-400 text-slate-50 text-4xl rounded-xl">WEBSITE PROJECT</div>
-    //   </div>
-    //   <div className="absolute top-[90px] -right-10 w-[320px] h-[180px] bg-slate-300 rounded-xl shadow-xl">
-    //     Cool Preview Here
-    //     <div className="absolute w-[90px] py-2 right-10 -bottom-2 bg-yellow-400 text-slate-50 text-xl rounded-xl">Cool</div>
-    //   </div>
-    //   <div className="absolute top-[310px] right-20 w-[170px] h-[270px] bg-slate-300 rounded-xl shadow-xl">
-    //     Mobile Preview Here
-    //     <div className="absolute w-[140px] py-2 -right-16 bottom-7 bg-yellow-400 text-slate-50 text-xl rounded-xl">Responsive</div>
-    //   </div>
-    // </div>
   );
 }
