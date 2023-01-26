24c5c0f5c7cb4de2f5ae80d8d4fcbe8b98052c43
Agung Saputra <agungsaputradifh@gmail.com>
2023-01-26 20:46:01 +0700

    fix px py about

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,6 +1,6 @@
 export default function Agent() {
   return (
-    <div className="pb-28 pt-24 px">
+    <div className="pb px">
       <h1 className="pt-20 pb-16 text-slate-900 font-bold text-4xl text-center">OUR PROJECT</h1>
 
       <div className="grid xl:grid-cols-3 2xl:grid-cols-3 mx-0 sm:mx-72 xl:mx-0 place-items-center gap-7 grid-cols-1">
