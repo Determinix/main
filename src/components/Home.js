fbc49e954e4f1561b58082e6391a4d52c2aa2533
Agung Saputra <agungsaputradifh@gmail.com>
2023-02-11 14:59:20 +0700

    remove image

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,11 @@
 import home1 from "./img/home/home1.jpeg";
 import home2 from "./img/home/home2.jpeg";
 import home3 from "./img/home/home3.jpeg";
-import home4 from "./img/home/home4.jpeg";
-import home5 from "./img/home/home5.jpeg";
 
 export default function Home() {
   return (
-    <div className="pt-20">
-      <img src={home3} />
+    <div className="">
+      <img src={home2} />
     </div>
   );
 }
