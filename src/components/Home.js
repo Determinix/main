e45d0bc5e9854513a28f2dddafa81f1a28d26714
Agung Saputra <agungsaputradifh@gmail.com>
2023-02-11 14:54:23 +0700

    add all img to home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,13 @@
-import image from "./img/home/home3.jpeg";
+import home1 from "./img/home/home1.jpeg";
+import home2 from "./img/home/home2.jpeg";
+import home3 from "./img/home/home3.jpeg";
+import home4 from "./img/home/home4.jpeg";
+import home5 from "./img/home/home5.jpeg";
 
 export default function Home() {
   return (
     <div className="pt-20">
-      <img src={image} />
+      <img src={home3} />
     </div>
   );
 }
